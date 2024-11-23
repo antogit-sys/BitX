a46be1edf476087931e8fa42f0534768bd7935cc
antogit-sys <antonioperrgiorgi@gmail.com>
2024-11-23 10:31:11 +0100

    fix indentation 1

--- include/BitX.h
+++ include/BitX.h
@@ -1,14 +1,14 @@
 #ifndef __BITX_H
-#	define __BITX_H
-#	include <stdint.h>
+#   define __BITX_H
+#   include <stdint.h>
 
 /* bit wise operator */
-#define AND(a, b)	((a) & (b))
-#define OR(a, b)	((a) | (b))
-#define NOT(a)		(~(a))
-#define XOR(a, b)	((a) ^ (b))
-#define LSHIFT(a)	((a) << 1)
-#define RSHIFT(a)	((a) >> 1)
+#define AND(a, b)   ((a) & (b))
+#define OR(a, b)    ((a) | (b))
+#define NOT(a)      (~(a))
+#define XOR(a, b)   ((a) ^ (b))
+#define LSHIFT(a)   ((a) << 1)
+#define RSHIFT(a)   ((a) >> 1)
 
 /* subsection bit wise operator */
 #endif
