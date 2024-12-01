9f53b409063bc3862bf72925be85870d64432f09
antogit-sys <antoperrgiorgi@gmail.com>
2024-12-01 15:39:16 +0100

    add multiplexer void

--- src/BitX.c
+++ src/BitX.c
@@ -3,11 +3,19 @@
 
 endian_t getEndian(void){
     endian_t e = BIG_ENDIAN;
-    word w = INIT_SEQ(0x12AB); //w.base = 0x12AB;
+    ComplexWORD cw = INIT_SEQ(0x12AB); //w.base = 0x12AB;
 
-    if(w.oct1 == 0x12  &&  w.oct2 == 0xAB)
+    if(cw.oct1 == 0x12  &&  cw.oct2 == 0xAB)
         e = LITTLE_ENDIAN;
 
     return e;
 }
 
+void* multiplexer(const void* arr, size_t sel, size_t dim, size_t data_size){
+    void* done = NULL;
+
+    if(sel < dim)
+        done = (void*)arr + sel*data_size; //arr + offset
+
+    return done;
+}
